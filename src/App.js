edcada9f2d3e314530e164c58cd30f90fef4100b
abdessamadElkhyat <Elkhyat.abdessamad@gmail.com>
2025-05-19 00:12:13 +0100

    version final 4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Photos from "./pages/Photos";
 import "./photo.css"
-import "./app.css"
+import "./App.css"
 
 function App() {
   return (
